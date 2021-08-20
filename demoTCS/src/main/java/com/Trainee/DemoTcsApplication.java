096d10286540f44735bdb14c68da34eeb3d6d0fc
Stalyn-1994 <61216816+Stalyn-1994@users.noreply.github.com>
2021-08-19 19:35:27 -0500

    Add files via upload

---
+++ demoTCS/src/main/java/com/Trainee/DemoTcsApplication.java
@@ -0,0 +1,13 @@
+package com.Trainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTcsApplication.class, args);
+	}
+
+}
