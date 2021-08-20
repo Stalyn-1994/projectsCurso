5b172920afd74fcf849e5974cdce4b1376a17775
Stalyn-1994 <stalyn.vaz@gmail.com>
2021-08-19 22:39:36 -0500

    Izack:version1
    Version_1

--- demoTCS/src/main/java/com/Trainee/repository/ServicioTransaccion.java
+++
@@ -1,9 +0,0 @@
-package com.Trainee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Trainee.model.Transacction;
-
-public interface ServicioTransaccion extends JpaRepository<Transacction, Long>{
-
-}
